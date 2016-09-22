f8f78ab9f15ec61fa9d8c0da4d571abb9f5e9e20
leiqunying <leiqunying993730@nd.com.cn>
2016-09-22 16:32:21 +0800

    添加文件

---
+++ 我的文件/nd_kcb_iosHelper.h
@@ -0,0 +1,31 @@
+//
+//  UIModuleHelper.h
+//  UIModule
+//
+//  Created by devp on 5/12/15.
+//  Copyright (c) 2015 ND. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "KCBTermSetting.h"
+
+#define SCREEN_WIDTH  ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT  ([UIScreen mainScreen].bounds.size.height)
+
+@interface nd_kcb_iosHelper : NSObject
+
++ (NSString *)localizedStringForKey:(NSString *)key inBundle:(NSBundle *)bundle;
++ (NSString *)localizedStringForKey:(NSString *)key;
+
++ (NSString *)imagePathFor:(NSString *)baseImageName withTargetScreenScale:(CGFloat)scale inBundle:(NSBundle *)bundle;
++ (UIImage *)imageNamed:(NSString *)name;
+
++ (NSString *)colorHexStringNamed:(NSString *)name;
++ (UIColor *)colorWithKey:(NSString *)key;
+
++ (BOOL)isSameDayAsServerDate:(NSInteger)dayIndex inWeek:(NSInteger)weekNumber withPlans:(NSDictionary *)plans withTermSetting:(KCBTermSetting *)termSetting;
+
++ (BOOL)isInSchoolTerm:(KCBTermSetting *)termSetting;
+
+@end
